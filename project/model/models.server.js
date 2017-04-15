0ae356a176e0f5c305a30b8c983ec953ccb9244f
Lalith Kumar Ramamoorthy <lalithram@ccs.neu.edu>
2017-04-15 10:41:02 -0400

    user page image fix

--- project/model/models.server.js
+++ project/model/models.server.js
@@ -1,18 +1,13 @@
 module.exports = function () {
     var userModel = require("../model/user/user.model.server.js")();
     var reviewModel = require("../model/restaurant_home/review.model.server.js")();
-    // var pageModel = require("page/page.model.server.js")();
-    // var widgetModel = require("widget/widget.model.server.js")();
 
     var model = {
         userModel: userModel,
         reviewModel: reviewModel
-        // pageModel: pageModel,
-        // widgetModel: widgetModel
     };
     reviewModel.reviewModelDependency(model.userModel);
-    // pageModel.pageModelDependency(model.websiteModel);
-    // widgetModel.widgetModelDependency(model.pageModel);
+
 
     return model;
 };
